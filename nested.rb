8a4db0e041c17295711c4cd86b49d05c54ffe592
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-06-25 22:41:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,5 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-		programmer_hash[:dennis_ritchie][:known_for]
+		programmer_hash[:dennis_ritchie][:known_for][1] = "Assembly"
+		programmer_hash
 end
